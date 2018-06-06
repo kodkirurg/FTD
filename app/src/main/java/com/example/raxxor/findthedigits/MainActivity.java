586e6737a74ca58c7736fcb180034d9b258a40a4
Johannes(raxxor) <johannes9533@hotmail.com>
2018-06-06 02:13:16 +0200

    new project

---
+++ app/src/main/java/com/example/raxxor/findthedigits/MainActivity.java
@@ -0,0 +1,13 @@
+awpackage com.example.raxxor.findthedigits;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
